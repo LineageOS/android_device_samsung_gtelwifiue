1947746cc56bb95cd5605db2cc17ff75ab6f6da8
andrewgreen5610 <andrewgreen5610@gmail.com>
2018-12-06 07:26:14 -0500

    Update and rename lineage.mk to omni.mk

--- omni.mk
+++ omni.mk
@@ -1,12 +1,12 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/gte-common/lineage.mk)
+$(call inherit-product, device/samsung/gte-common/omni.mk)
 
 $(call inherit-product, device/samsung/gtelwifiue/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gtelwifiue
-PRODUCT_NAME := lineage_gtelwifiue
+PRODUCT_NAME := omni_gtelwifiue
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T560NU
 PRODUCT_MANUFACTURER := samsung
